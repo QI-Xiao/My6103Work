ffa6c249d7f537a58fcec20b03c0efc62eb9cea3
QI-Xiao <1993qixiao@gmail.com>
2022-01-17 00:13:10 -0500

    change some values in the Hello python file and .gitignore

--- Hello_Xiao.py
+++ Hello_Xiao.py
@@ -1,15 +1,15 @@
 #%%
 print("Hello world")
-print(5 / 8)
-print (7+10)
-print(10/3, 3/10) # print numerical divisions
+print(32)
+print (7e+10)
+print(10/33, 3/10) # print numerical divisions
 print(10//3, 3//10) # print quotients from divisions
 print(10%3, 3%10) # print remainders from divisions
 
 #%%
 astring = "Thank you"
-anum = 3.14159265358979323846
-cnt = 1
+anum = 112358
+cnt = 10
 # Many different ways to print out the same line
 print("%d. I want to say %s" % (cnt,astring) )
 cnt+=1
